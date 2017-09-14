4f322432b0a40917431473f1e25a4a50b1aca7e6
udaykumark2204 <31339846+udaykumark2204@users.noreply.github.com>
2017-09-14 19:53:51 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -4,3 +4,8 @@ data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
 data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
 hist(data1$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+
+## Saving to file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
