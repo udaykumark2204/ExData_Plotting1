af44b46a2f83cc2d0224b0e6d32189b6201d1283
udaykumark2204 <31339846+udaykumark2204@users.noreply.github.com>
2017-09-14 19:53:21 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -10,3 +10,8 @@ with(data1, {
   plot(Global_active_power~Datetime, type="l",
      ylab="Global Active Power (kilowatts)", xlab="")
   })
+
+
+## Saving to file
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
